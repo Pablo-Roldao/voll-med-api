bd5c75681c279b4173adc98c7c904a3fbb11d477
Pablo Roldão <pablo.roldao.santos@gmail.com>
2024-09-07 22:47:50 -0300

    feat: http responses with good practices

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String olaMundo() {
-        return "Hello World!";
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
